aa63aac19c66d5efe7a629dd173bbe0e1e17d343
kim <youoper@gmail.com>
2020-09-03 15:58:55 +0900

    bot response added

---
+++ kakao/bot.py
@@ -0,0 +1,16 @@
+
+def simpleText(text):
+    response_dict = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": text
+                    }
+                }
+            ]
+        }
+    }
+    return response_dict
+
--- kakao/views.py
+++ kakao/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from . import api
+from . import api, bot
 import json
 
 # Create your views here.
 
+
 def translate(request):
     request_json = json.loads(request.body)
-    text = request_json['action']['params']['text']['value']
-    return JsonResponse(api.kr_to_en(text))
+    request_text = request_json['action']['params']['text']['value']
+    response_text = api.kr_to_en(request_text)
+    response_dict = bot.simpleText(response_text)
+    return JsonResponse(response_dict)
